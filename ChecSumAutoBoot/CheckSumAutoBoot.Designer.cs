8cab1a585876ba40bcaf59274bdbc5b808a530d9
ArifOmer24 <arifomersicak37@gmail.com>
2024-10-14 07:22:13 +0300

    added

---
+++ ChecSumAutoBoot/CheckSumAutoBoot.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace ChecSumAutoBoot
+{
+    partial class CheckSumAutoBoot
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(892, 172);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(354, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(892, 234);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(354, 23);
+            this.progressBar2.TabIndex = 1;
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.Location = new System.Drawing.Point(892, 298);
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.Size = new System.Drawing.Size(354, 23);
+            this.progressBar3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(570, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(388, 39);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "CheckSumAutoBoot.exe";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(429, 282);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(238, 39);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Internet Check";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(429, 218);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(260, 39);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "DataLine Check";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(429, 156);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(238, 39);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "RegEdit Install";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(429, 387);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 39);
+            this.label5.TabIndex = 7;
+            // 
+            // CheckSumAutoBoot
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1487, 809);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar3);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "CheckSumAutoBoot";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.ProgressBar progressBar3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ ChecSumAutoBoot/CheckSumAutoBoot.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using Microsoft.Win32;
+using System.IO;
+using System.Runtime.InteropServices;
+using Microsoft.VisualBasic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Reflection.Emit;
+using System.Linq;
+
+namespace ChecSumAutoBoot
+{
+    public partial class CheckSumAutoBoot : Form
+    {
+
+        public CheckSumAutoBoot()
+        {
+            InitializeComponent();
+            this.KeyPreview = true; // Tuş vuruşlarını formda yakala
+            this.WindowState = FormWindowState.Maximized; // Formu tam ekran yap
+            this.FormBorderStyle = FormBorderStyle.None; // Kenarlıkları kaldır
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StartProcess();
+            DisableStartMenu();
+            HideSystemFolders();
+            DisableRightClick();
+            DisableTaskManager();
+            DisableControlPanel();
+            DisableRecycleBin();
+            RestartExplorer();
+
+        }
+
+
+
+        private async void StartProcess()
+        {
+            await Task.Run(() => RunProcess(progressBar1, "RegEdit Install", 1000));
+            await Task.Run(() => RunProcess(progressBar2, "DataLine Check", 1500));
+            await Task.Run(() => RunProcess(progressBar3, "Internet Check", 2000));
+            CheckAndLogInternet();
+            // Başka bir uygulamayı çalıştır
+            Process.Start("C:\\Program Files (x86)\\Electrum");
+
+            Application.Exit();
+
+        }
+
+
+
+        private void RunProcess(ProgressBar progressBar, string labelText, int duration)
+        {
+            // Label metnini güncelle
+            Invoke((MethodInvoker)delegate
+            {
+                label5.Text = labelText;
+            });
+
+            for (int i = 0; i <= 100; i++)
+            {
+                // Progress bar değerini güncelle
+                Invoke((MethodInvoker)delegate
+                {
+                    progressBar.Value = i;
+                });
+
+                // Bir süre bekleyerek ilerlemeyi simüle et
+                Thread.Sleep(duration / 100);
+            }
+
+            // İşlem tamamlandıktan sonra label'ı güncelle
+            Invoke((MethodInvoker)delegate
+            {
+                label5.Text = labelText + " done!";
+            });
+        }
+
+
+
+
+
+
+
+        void DisableStartMenu()
+        {
+            RegistryKey key = Registry.CurrentUser.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Policies\Explorer");
+            key.SetValue("NoStartMenu", 1, RegistryValueKind.DWord);
+            key.Close();
+        }
+
+        void HideSystemFolders()
+        {
+            RegistryKey key = Registry.CurrentUser.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Policies\Explorer");
+            key.SetValue("NoDesktop", 1, RegistryValueKind.DWord);
+            key.Close();
+        }
+
+        void DisableRightClick()
+        {
+            RegistryKey key = Registry.CurrentUser.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Policies\Explorer");
+            key.SetValue("NoViewContextMenu", 1, RegistryValueKind.DWord);
+            key.Close();
+        }
+
+        void DisableTaskManager()
+        {
+            RegistryKey key = Registry.CurrentUser.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Policies\System");
+            key.SetValue("DisableTaskMgr", 1, RegistryValueKind.DWord);
+            key.Close();
+        }
+
+        void DisableControlPanel()
+        {
+            RegistryKey key = Registry.CurrentUser.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Policies\Explorer");
+            key.SetValue("NoControlPanel", 1, RegistryValueKind.DWord);
+            key.Close();
+        }
+
+        void DisableRecycleBin()
+        {
+            RegistryKey key = Registry.CurrentUser.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Policies\Explorer");
+            key.SetValue("NoRecycleBin", 1, RegistryValueKind.DWord);
+            key.Close();
+        }
+
+        void RestartExplorer()
+        {
+            Process[] explorerProcesses = Process.GetProcessesByName("explorer");
+            foreach (var process in explorerProcesses)
+            {
+                process.Kill();
+            }
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Escape)
+            {
+                EnableTaskManager();
+                EnableControlPanel();
+                EnableRecycleBin();
+                RestoreStartMenu();
+                ShowSystemFolders();
+                EnableRightClick();
+                RestartExplorer();  // Değişikliklerin etkili olması için Explorer'ı yeniden başlatın
+                MessageBox.Show("Tüm değişiklikler geri alındı.");
+            }
+
+        }
+
+
+        void EnableTaskManager()
+        {
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Policies\System", true);
+            if (key != null)
+            {
+                key.DeleteValue("DisableTaskMgr", true);
+                key.Close();
+            }
+        }
+
+        void EnableControlPanel()
+        {
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Policies\Explorer", true);
+            if (key != null)
+            {
+                key.DeleteValue("NoControlPanel", true);
+                key.Close();
+            }
+        }
+
+        void EnableRecycleBin()
+        {
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Policies\Explorer", true);
+            if (key != null)
+            {
+                key.DeleteValue("NoRecycleBin", true);
+                key.Close();
+            }
+        }
+
+        void ShowSystemFolders()
+        {
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Policies\Explorer", true);
+            if (key != null)
+            {
+                key.DeleteValue("NoDesktop", true);
+                key.Close();
+            }
+        }
+
+        void EnableRightClick()
+        {
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Policies\Explorer", true);
+            if (key != null)
+            {
+                key.DeleteValue("NoViewContextMenu", true);
+                key.Close();
+            }
+        }
+        void RestoreStartMenu()
+        {
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Policies\Explorer", true);
+            if (key != null)
+            {
+                key.DeleteValue("NoStartMenu", true);
+                key.Close();
+            }
+        }
+
+
+
+
+
+
+
+        // İnternet işlemleri
+
+
+        public static bool CheckInternetConnection()
+        {
+            try
+            {
+                using (var client = new System.Net.WebClient())
+                using (client.OpenRead("http://www.google.com"))
+                {
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+
+        private void DisableActiveNetwork()
+        {
+            var activeNetwork = NetworkInterface.GetAllNetworkInterfaces()
+                .FirstOrDefault(n => n.OperationalStatus == OperationalStatus.Up &&
+                                     (n.NetworkInterfaceType == NetworkInterfaceType.Wireless80211 ||
+                                      n.NetworkInterfaceType == NetworkInterfaceType.Ethernet));
+
+            if (activeNetwork != null)
+            {
+                // Ağ bağlantısını devre dışı bırak
+                DisableNetwork(activeNetwork.Name);
+            }
+        }
+        private void EnableActiveNetwork()
+        {
+            var activeNetwork = NetworkInterface.GetAllNetworkInterfaces()
+                .FirstOrDefault(n => n.OperationalStatus == OperationalStatus.Down &&
+                                     (n.NetworkInterfaceType == NetworkInterfaceType.Wireless80211 ||
+                                      n.NetworkInterfaceType == NetworkInterfaceType.Ethernet));
+
+            if (activeNetwork == null)
+            {
+                // Ağ bağlantısını etkinleştir
+                EnableNetwork(activeNetwork.Name);
+            }
+        }
+
+
+        public static void DisableNetwork(string networkName)
+        {
+            Process.Start("netsh", $"interface set interface \"{networkName}\" admin=disabled");
+        }
+
+        public static void EnableNetwork(string networkName)
+        {
+            Process.Start("netsh", $"interface set interface \"{networkName}\" admin=enabled");
+        }
+
+        public static void LogStatus(int status)
+        {
+            string logFilePath = @"C:\Users\Arif\source\repos\ChecSumAutoBoot\ChecSumAutoBoot\Checksum.txt";
+            File.AppendAllText(logFilePath, status.ToString() + Environment.NewLine);
+        }
+
+        private void CheckAndLogInternet()
+        {
+            int status = CheckInternetConnection() ? 1 : 0;
+            LogStatus(status);
+
+            if (status == 1)
+                DisableActiveNetwork();
+        }
+    }
+
+
+}
